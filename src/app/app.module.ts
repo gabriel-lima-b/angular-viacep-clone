f523af5ecefbc1d35186f1bd80efdb4f808e7936
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-11-04 14:39:14 -0300

    removed parts of the project that were used as references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
 import { CepFinderComponent } from './cep-finder/cep-finder.component';
 import { CepListComponent } from './cep-list/cep-list.component';
 import { CepDetailsComponent } from './cep-details/cep-details.component';
@@ -22,20 +16,13 @@ import { CepDetailsComponent } from './cep-details/cep-details.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: CepFinderComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'all', component: CepListComponent },
-      { path: 'shipping', component: ShippingComponent },
       {path: 'details/:cep', component: CepDetailsComponent},
     ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent,
     CepFinderComponent,
     CepListComponent,
     CepDetailsComponent,
